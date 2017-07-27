59bc8b801eab0623a89a007de7330924f9516e80
Juliet Slade <juliet.slade@gmail.com>
2017-07-27 17:33:34 -0400

    Edited/reworded content, mostly cleaned up CSS, added "about me" page, updated resume, experimentally commented out dropdown menus

--- project-scripts/slides.js
+++ project-scripts/slides.js
@@ -1,48 +1,23 @@
 var slideIndex = 1;
-var slideIndex2 = 1;
-var slideIndex3 = 1;
+showSlides(slideIndex);
 
-function plusSlides(n, name) {
-  if (name === "mySlides") {
-    showSlides(slideIndex += n, name);
-  } else if (name === "mySlides2"){
-    showSlides(slideIndex2 +=n, name);
-  } else {
-    showSlides(slideIndex3 += n, name);
-  }
+function plusSlides(n) {
+  showSlides(slideIndex += n, name);
 }
 
 
 
-function showSlides(n, name) {
+function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName(name);
+  var slides = document.getElementsByClassName('mySlides');
   if (n > slides.length) {
-    if (name === "mySlides") {
-      slideIndex = 1;
-    } else if (name === "mySlides2") {
-      slideIndex2 = 1;
-    } else {
-      slideIndex3 = 1;
-    }
+    slideIndex = 1;
   }
   if (n < 1) {
-    if (name === "mySlides") {
-      slideIndex = slides.length;
-    } else if (name === "mySlides2") {
-      slideIndex2 = slides.length;
-    } else {
-      slideIndex3 = slides.length;
-    }
+    slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  if (name === "mySlides") {
-    slides[slideIndex-1].style.display = "block";
-  } else if (name === "mySlides2"){
-    slides[slideIndex2-1].style.display = "block";
-  } else {
-    slides[slideIndex3-1].style.display = "block";
-  }
+  slides[slideIndex-1].style.display = "block";
 }
