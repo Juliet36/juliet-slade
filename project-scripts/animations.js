5624b00cf64fd75ea3baf6f19d4fdb31c4716072
Juliet Slade <juliet.slade@gmail.com>
2017-08-04 16:17:48 -0400

    added some jQuery (project name motion is animated), resume sections collapse in an accordion, cleaned up some redundant CSS

---
+++ project-scripts/animations.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('table tr td').hover(function() {
+    $('~ .project-name h3', this).animate({left:"+=25px"}, 300);
+  },
+  function() {
+    $('~ .project-name h3', this).animate({left:"-=25px"}, 200);
+  });
+  $('.resume-section').accordion({collapsible:true});
+});
